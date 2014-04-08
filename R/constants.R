d686c6284ad9053fe65f9fde788822ea45be356d
aplsek <aplsek@gmail.com>
2014-04-07 20:41:28 -0700

    adding headers for ParOld

--- R/constants.R
+++ R/constants.R
@@ -1,5 +1,5 @@
 
-GC_TYPES <- c("g1-young", "g1evac", "g1full", "g1-young_to-spaceexhausted", "g1-young_initial-mark_to-spaceexhausted", "g1-young_initial-mark", "g1-mixed")
+GC_TYPES <- c("g1-young", "g1evac", "g1full", "g1-young_to-spaceexhausted", "g1-young_initial-mark_to-spaceexhausted", "g1-young_initial-mark", "g1-mixed", "ParOld-minor", "ParOld-full")
 colors <- c("blue", "green", "red", "dark red", "dark red", "yellow", "brown")
 #GC_TYPES <-  data.frame(colors,types)
 
