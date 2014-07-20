762caf5ead4820fb378f5167a3ad6d293ca1c1f5
aplsek <aplsek@gmail.com>
2014-07-20 14:33:41 -0700

    added a new GC type ParOld-full-meta

--- R/main.R
+++ R/main.R
@@ -8,12 +8,12 @@ source('R/style.R')
 inFile = commandArgs(trailingOnly=TRUE)[1]
 data.file = commandArgs(trailingOnly=TRUE)[2]
 report.id = commandArgs(trailingOnly=TRUE)[3]
-outFile = commandArgs(trailingOnly=TRUE)[4]
+#outFile = commandArgs(trailingOnly=TRUE)[4]
 
 
-
-
-data.file <- "data.txt"
+#data.file <- args[1]
+#data.file <- "data.txt"
+#data.file <- "dataParOld.txt"
 
 outFile.stats <- "gc.stats.txt"
 outFile <- "table.out.txt"
@@ -21,10 +21,11 @@ outFile2 <- "table-gctype.out.txt"
 data.gc <- read.csv(data.file,sep=',', header=TRUE)
 data.gc <- computeAllocationRate(data.gc)
 write.csv(file=outFile.stats, x=data.gc)
+#write.csv(file=outFile, x=data.gc)
 
 df <-statsMain(data.gc)
 dd <- as.data.frame(df)
-write.csv(file=df, x=dd)
+write.csv(file=outFile, x=dd)
 
 df <-statsByGCType(data.gc)
 
